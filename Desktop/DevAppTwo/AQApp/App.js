099ce1a9b9832ec9c17c174243e657f85222d849
¨FrancoGatti¨ <fgattidelaconcepcion@gmail.com>
2025-03-17 22:52:07 -0300

    Trabajando en la interfaz

--- Desktop/DevAppTwo/AQApp/App.js
+++ Desktop/DevAppTwo/AQApp/App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Text, View, Button, ImageBackground, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('./assets/ImgFondo.jpg')} // Ruta de la imagen
+      style={styles.contenedor} // Estilo extraído al objeto styles
+      resizeMode="cover" // Asegura que la imagen cubra todo el fondo
+    >
+      {/* Header con el texto en la parte superior */}
+      <View style={styles.header}>
+        <Text style={styles.titulo}>AQFIT</Text>
+      </View>
+      
+      {/* TextInput con fondo transparente y texto blanco */}
+      <TextInput
+        style={styles.input}
+        placeholder='Escribe algo en el input'
+        placeholderTextColor='gray'
+      />
+      
+      <Button
+        title='Presiona aqui'
+        onPress={() => alert('Botón presionado')}
+        color='white'
+      />
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contenedor: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'flex-start', // Esto asegura que los componentes estén alineados desde el principio
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    height: 80, // Espacio para el header
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo transparente para el header
+    width: '100%',
+    justifyContent: 'center', // Centra el texto dentro del header verticalmente
+    alignItems: 'center', // Centra el texto horizontalmente
+  },
+  titulo: {
+    fontSize: 50,
+    color: 'white',
+  },
+  input: {
+    color: 'white',
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
+    width: '80%',
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo transparente con algo de opacidad
   },
 });
