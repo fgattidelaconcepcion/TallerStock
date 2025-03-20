e5cf712b15b0d334e37b56fee094c23ee6bc5ddf
¨FrancoGatti¨ <fgattidelaconcepcion@gmail.com>
2025-03-20 00:31:38 -0300

    Eventos y listas

--- Desktop/DevAppTwo/AQApp/App.js
+++ Desktop/DevAppTwo/AQApp/App.js
@@ -1,6 +1,38 @@
+import { useState } from 'react';
 import { TextInput, Text, View, Button, ImageBackground, StyleSheet } from 'react-native';
 
+const productsExample = [
+  { id: 1, value: "Mate" },
+  { id: 2, value: "Cafe" },
+  { id: 3, value: "Harina" },
+  { id: 4, value: "Palmitos" },
+  { id: 5, value: "Yerba" },
+  { id: 6, value: "Mermelada" },
+  { id: 7, value: "Cacao" },
+  { id: 8, value: "Picadillo" },
+  { id: 9, value: "Pate" },
+  { id: 10, value: "Caballa" },
+  { id: 11, value: "Arroz" },
+  { id: 12, value: "Arvejas" },
+  { id: 13, value: "Sadinas" },
+  { id: 14, value: "Atun" },
+  { id: 15, value: "Choclo" },
+  { id: 16, value: "Lentejas" },
+];
+
 export default function App() {
+
+  const [textItem,setTextItem] = useState('')
+  const [itemList,setItemList] = useState(productsExample)
+
+   const handleChangeText = (text) => {
+    console.log(text)
+    setTextItem(text)
+   }
+   const addItem = () => {
+    console.log ('add Item')
+   }
+
   return (
     <ImageBackground
       source={require('./assets/ImgFondo.jpg')} // Ruta de la imagen
@@ -13,7 +45,8 @@ export default function App() {
       </View>
       
       {/* TextInput con fondo transparente y texto blanco */}
-      <TextInput
+      <TextInput value = {textItem}
+      onChangeText={handleChangeText}
         style={styles.input}
         placeholder='Escribe algo en el input'
         placeholderTextColor='gray'
@@ -21,10 +54,19 @@ export default function App() {
       
       <Button
         title='Presiona aqui'
-        onPress={() => alert('Botón presionado')}
+        onPress={(addItem)}
         color='white'
       />
-    </ImageBackground>
+
+<View>
+    {itemList.map((item) => (
+      <View key={item.id}>
+        <Text>{item.value}</Text>
+      </View>
+    ))}
+  </View>
+</ImageBackground>
+
   );
 }
 
@@ -33,6 +75,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start', // Esto asegura que los componentes estén alineados desde el principio
     alignItems: 'center',
+    paddingVertical : 20,
   },
   header: {
     height: 80, // Espacio para el header
